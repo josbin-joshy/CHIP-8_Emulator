a2527f806624074d905369c419214ef200ce246b
josbin <joshyjosbin@gmail.com>
2026-06-27 19:31:02 +0530

    added some instructions

--- source/chip8.cpp
+++ source/chip8.cpp
@@ -6,6 +6,11 @@
 #include"chip8.hpp"
 
 
+constexpr uint16_t START_ADDRESS{0x200};
+constexpr uint16_t FONTSET_START_ADDRESS {0x50};
+static constexpr unsigned int FONTSET_SIZE{80};
+
+
 uint8_t fontset[FONTSET_SIZE] = 
     {
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -47,6 +52,41 @@ Chip8::Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().cou
 
 }
 
+void Chip8::loadROM(const char* filename)
+{
+    //open file as binary stream and move fp to the end
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if(file.is_open())
+    {
+        //basically read the no of bytes and created a array of that size
+        std::streamsize size{file.tellg()};
+
+        //checking for overflow
+        if(size > 4096-START_ADDRESS) return;
+
+        //creating the array of that size
+        char* buffer{new char[size]};
+
+        //reading the file onto the new array 
+        //first setting fp to start so that it can read
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+
+        //closing it cause i odnt need it anymore :)---
+        file.close();
+
+        //now we load it muhahah
+        for(long i = 0; i < size; ++i)
+        {
+            memory[START_ADDRESS+i] = buffer[i];
+        }
+
+        //freeeeeeeeeeeeeee we dont like memory leaks
+        delete[] buffer;
+    }
+    
+}
 
 void Chip8::OP_00E0()
 {
