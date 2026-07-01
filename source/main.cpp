c13032cd7faeb90149f8c157bb319310fd8d4c50
josbin <joshyjosbin@gmail.com>
2026-07-01 21:29:31 +0530

    made some changes to main

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,24 @@
 #include"chip8.hpp"
-#include<string>
+#include<iostream>
 
 Chip8 chip8;
 
 int main(int argc, char** argv)
 {
+    if(argc < 2) 
+    {
+        std::cerr << "usage: " << argv[0] << "<chip8ROM>\n";
+        return 1;
+    }
+
     const char* filename{argv[argc-1]};
     chip8.loadROM(filename);
+
+    bool runflag{true};
+    while(runflag)
+    {
+
+    }
+
+    return 0;
 }
